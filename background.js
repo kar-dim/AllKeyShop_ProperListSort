5143f70f538f396153e9fb7fd38b54f4faa5d028
Mimis <dim_kar@outlook.com>
2025-02-11 17:57:03 +0200

    fixup, AKS page changed html

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//execute the script after clicking the extension icon
 browser.browserAction.onClicked.addListener(function(tab) {
     browser.tabs.executeScript(tab.id, {file: "main.js"})
         .catch(console.error);
--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-function sortLowestPrices() {
+//retrieve game price
+const getGamePrice = (row) => {
+    const aElement = row.children[row.children.length - 2].querySelector("a");
+    return aElement ? parseFloat(aElement.textContent.trim().slice(0, -1)) : NaN;
+};
+
+//get all the game rows, and sort them
+const sortLowestPrices = () => {
 	const gamesTable = document.querySelector('table.akswl-list');
 	const gamesBody = gamesTable && gamesTable.querySelector('tbody');
 	const gamesRows = gamesBody && Array.from(gamesBody.querySelectorAll('tr'));
+	//no valid AKS page, exit
 	if (!gamesTable || !gamesBody || !gamesRows) {
 		alert('No AKS list detected!');
 		return;
 	}
 
+	//sort all the found game rows
     gamesRows.sort((a, b) => {
-		const priceA = parseFloat(a.querySelector('td.game-lowest-price a').textContent.trim().slice(0,-1));
-		const priceB = parseFloat(b.querySelector('td.game-lowest-price a').textContent.trim().slice(0,-1));
+		const priceA = getPrice(a);
+        const priceB = getPrice(b);
 		if ((isNaN(priceA) && isNaN(priceB)) || isNaN(priceA))
 			return 1;
 		if (isNaN(priceB))
@@ -17,8 +26,10 @@ function sortLowestPrices() {
         return priceA - priceB;
     });
 
+	//clear game rows content and put the sorted games
     gamesBody.innerHTML = '';
     gamesRows.forEach(gameRow => gamesBody.appendChild(gameRow));
 }
 
+//run the sort function
 sortLowestPrices();
